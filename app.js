9152d2b60a9864016a46511ca7acd878bfdcc454
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2023-09-14 23:00:28 +0200

    setup: enable cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const divesRouter = require('./routes/api/v1/dives.js');
@@ -23,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/dives', divesRouter);
